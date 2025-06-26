10be22492199156e35197601eddb204319ebfa98
Khunakorn Arada <khunakorn551@example.com>
2025-06-26 23:01:02 +0700

    m

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -111,18 +111,18 @@
                 </div>
 
                 <!-- Main Content -->
-                <div class="flex-1 flex flex-col max-w-full" :class="sidebarOpen ? 'lg:ml-52' : 'ml-0'">
+                <div class="flex-1 flex flex-col max-w-full lg:ml-0">
                     <!-- Page Heading -->
                     @isset($header)
                         <header class="bg-white shadow mb-4">
-                            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 lg:px-0">
                                 <h1 class="text-2xl font-bold text-gray-900 border-b border-gray-200 pb-2">{{ $header }}</h1>
                             </div>
                         </header>
                     @endisset
 
                     <!-- Page Content -->
-                    <main class="flex-1 py-6 px-2 sm:px-4 lg:px-8 max-w-7xl mx-auto w-full overflow-x-hidden">
+                    <main class="flex-1 py-6 px-2 sm:px-4 lg:px-8 max-w-7xl mx-auto w-full overflow-x-hidden lg:px-0">
                         {{ $slot }}
                     </main>
 
