2397c73e269315988157934dccbc477ce6ba7194
Khunakorn Arada <khunakorn551@example.com>
2025-06-26 22:45:26 +0700

    m

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,5 +1,5 @@
 <nav x-data="{ sidebarOpen: false }" class="bg-white border-b border-gray-100">
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div class="max-w-7xl mx-auto px-4">
         <div class="flex justify-between h-16 items-center">
             <!-- Hamburger for sidebar (mobile only) -->
             <div class="flex items-center lg:hidden">
