7cb802b303a43c53ecdc2eb9f3af1792fb0466eb
Khunakorn <jan842070@gmail.com>
2025-07-13 18:20:22 +0700

    fix footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -3,7 +3,7 @@
         <div class="flex flex-col md:flex-row justify-between items-center">
             <div class="text-center md:text-left mb-4 md:mb-0">
                 <p class="text-sm text-gray-500">
-                    &copy; {{ date('Y') }} {{ config('app.name', 'Laravel') }}. All rights reserved.
+                    &copy; {{ date('Y') }} Karen Song Lyrics. All rights reserved.
                 </p>
             </div>
             <div class="flex space-x-6">
